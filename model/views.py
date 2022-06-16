e2a169c2d4e9730b9f65ceb42c238df2d06db47c
rafi <raffizz.muhammed@gmail.com>
2022-06-16 11:29:38 +0530

    Create login page and Register page

---
+++ model/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+
+def start(request):
+    return render(request,'index.html')
+
+
+def login(request):
+    return render(request,'login.html')
+
+
+def register(request):
+    return render(request,'register.html')
